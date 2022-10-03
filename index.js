7b673842f387cfd0e6254fc168e9c905a9d86545
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-03 16:49:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ function destructivelyPrependCat(name) {
 function destructivelyRemoveLastCat(name) {
     cats.pop();
 }
+
 function destructivelyRemoveFirstCat(name) {
     cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = ["Arnold",...cats];
+    return newCats;
+}
+
+function removeLastCat(name) {
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1);
 }
