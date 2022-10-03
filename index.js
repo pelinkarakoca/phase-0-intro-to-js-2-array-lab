c5aea881b46118ada9d11bf53a1cce322f04b468
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-03 16:23:09 +0300

    Testing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
